35afcb9c69262437562167b8685bca2bda591c8d
Otto Brinkhaus <brinkhaus@gmail.com>
2023-05-31 10:38:56 +0200

    initial commit with some first steps

---
+++ tests.py
@@ -0,0 +1,16 @@
+from cumulative_error_score import *
+
+import numpy as np
+import unittest
+
+
+class TestCesCalculation(unittest.TestCase):
+    def test_calculate_similarity_group_counts(self,):
+        similarities = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        expected = np.array([
+            5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9])
+        assert np.array_equal(calculate_similarity_group_counts(similarities), expected)
